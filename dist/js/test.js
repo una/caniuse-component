713d088c3fd17b16668f574e5006c17b09ecf65c
Una Kravets <una.kravets@gmail.com>
2016-07-07 22:57:59 -0500

    set up Babel

---
+++ dist/js/test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function Person(name) {
+  _classCallCheck(this, Person);
+
+  this.name = name;
+};
